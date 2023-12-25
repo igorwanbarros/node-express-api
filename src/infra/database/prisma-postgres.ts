dc637b4c117ae36e99fd6e25acf420a7ca9dc53e
Igor Wanderley <igorwanbarros@gmail.com>
2023-12-25 18:45:33 -0300

    feat: adding debug to prisma config

--- src/infra/database/prisma-postgres.ts
+++ src/infra/database/prisma-postgres.ts
@@ -1,7 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
+const debug = Boolean(process.env.APP_DEBUG)
+
 const prismaClient = new PrismaClient({
-  log: ['query', 'error', 'info', 'warn']
+  errorFormat: 'pretty',
+  log: debug ? [{ emit: 'event', level: 'query' }] : []
 });
+
+if (debug) {
+  prismaClient.$on("query", async ({query, params, duration}) => {
+    console.log({ query, params, duration: duration + 'ms' });
+  })
+}
 
 export default prismaClient;
